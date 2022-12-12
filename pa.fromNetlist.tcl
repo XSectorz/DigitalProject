2b4fd350e4f0d0befd05bbc573fd855691862981
XSectorz <81065047+XSectorz@users.noreply.github.com>
2022-12-13 03:08:05 +0700

    Lastest Released
    [ใช้งานได้แล้ว]

--- pa.fromNetlist.tcl
+++ pa.fromNetlist.tcl
@@ -1,11 +1,10 @@
 
-# PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
+# PlanAhead Launch Script for Post-Synthesis floorplanning, created by Project Navigator
 
-create_project -name DigitalFinalProject -dir "D:/Xsectorz/ISE/DigitalFinalProject/planAhead_run_2" -part xc6slx9tqg144-3
+create_project -name DigitalFinalProject -dir "D:/Xsectorz/ISE/DigitalFinalProject/planAhead_run_5" -part xc6slx9tqg144-3
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "D:/Xsectorz/ISE/DigitalFinalProject/VGADriver.ngc" [ get_property srcset [ current_run ] ]
+set_property edif_top_file "D:/Xsectorz/ISE/DigitalFinalProject/vga_driver.ngc" [ get_property srcset [ current_run ] ]
 add_files -norecurse { {D:/Xsectorz/ISE/DigitalFinalProject} }
-set_param project.pinAheadLayout  yes
-set_property target_constrs_file "VGADriver.ucf" [current_fileset -constrset]
-add_files [list {VGADriver.ucf}] -fileset [get_property constrset [current_run]]
+set_property target_constrs_file "PinDriver.ucf" [current_fileset -constrset]
+add_files [list {PinDriver.ucf}] -fileset [get_property constrset [current_run]]
 link_design
